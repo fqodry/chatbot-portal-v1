9411b00f1082853a8665c1b8d3c38cac2bdfd0da
Moh. Firman Qodry <firman.qodry@gmail.com>
2018-12-28 08:33:37 +0700

    Initial Base Project

---
+++ src/main/java/id/mbingweb/chatbotportalv1/ChatbotPortalV1Application.java
@@ -0,0 +1,14 @@
+package id.mbingweb.chatbotportalv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotPortalV1Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(ChatbotPortalV1Application.class, args);
+    }
+
+}
+
---
+++ src/main/java/id/mbingweb/chatbotportalv1/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package id.mbingweb.chatbotportalv1.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author Firman Qodry
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //TODO here
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+}
